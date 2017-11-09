ce3748f306a75b5042942e978e651b2d00f684e1
AntoineGourtayHetic <antoine.gourtay@hetic.net>
2017-11-09 17:52:35 +0100

    :construction: wip

--- qonto_client.rb
+++ qonto_client.rb
@@ -13,6 +13,14 @@ class QontoClient
         @url = 'https://thirdparty.qonto.eu/v1'
     end
 
+    def iban
+        organizations["organization"]["bank_accounts"][0]["iban"]
+    end
+
+    def slug_bank_account
+        organizations["organization"]["bank_accounts"][0]["slug"]
+    end
+
     #Méthode
     def organizations
         res = RestClient.get(@url + "/organizations/#{slug}",
@@ -22,31 +30,32 @@ class QontoClient
         JSON.parse(res.body)
     end
 
-    def transactions(slug_bank_account, iban)
-        res = RestClient.get(url + "/transaction?slug=#{slug_bank_account}&iban=#{iban}",
+    def call_transaction(next_page = 1)
+        res = RestClient.get(url + "/transactions?slug=#{slug_bank_account}&iban=#{iban}&params=#{next_page}",
                              {"Authorization" => "#{slug}:#{secret}"}
         )
 
-        puts res
         status = res.code
         JSON.parse(res.body)
     end
 
-    def iban
-        organizations["organization"]["bank_accounts"][0]["iban"]
+    def all_transactions(full_transactions = [], next_page = 1)
+
+        full_transactions << call_transaction["transactions"]
+
+        if (full_transactions["meta"]["total_count"]).to_i == 100
+            full_transactions << all_transactions(full_transactions, next_page: transacs["meta"]["next_page"])
+        end
+
+      all_transactions
+
     end
 
-    def slug_bank_account
-        organizations["organization"]["bank_accounts"][0]["slug"]
-    end
 
 end
 
+
 client = QontoClient.new('qonto-test-9312', 'b5e1cdfc34b0')
 
-slug = client.slug_bank_account
-iban = client.iban
-
-puts client.transactions(slug, iban)
-
-meta = client.transactions(slug, iban)
+transactions = client.all_transactions["transactions"]
+puts transactions.count
