2fc55041a40ed4123d2f0f0a2b50a8e8d3b938ac
AntoineGourtayHetic <antoine.gourtay@hetic.net>
2017-11-09 17:07:27 +0100

    :construction: adding a ward

--- qonto_client.rb
+++ qonto_client.rb
@@ -1,26 +1,37 @@
+require 'json'
 require 'rest-client'
+
 
 class QontoClient
 
     attr_reader :slug, :secret, :url
 
+    #COnstructeur
     def initialize(slug, secret)
         @slug = slug
         @secret = secret
         @url = 'https://thirdparty.qonto.eu/v1'
     end
 
+    #Méthode
     def organizations
         res = RestClient.get(@url + "/organizations/#{slug}",
                        {"Authorization" => "#{slug}:#{secret}"}
         )
-        
-        puts res.code
-        puts res.body
+        return "Erreur : #{res.status}" if res.code.to_i != 200
+        JSON.parse(res.body)
+    end
+
+    def iban
+        organizations["bank_accounts"][0]["iban"]
+    end
+
+    def slug_bank_account
+        organizations["bank_accounts"][0]["slug"]
     end
 
 end
 
-
 client = QontoClient.new('qonto-test-9312', 'b5e1cdfc34b0')
-client.organizations
+orga = client.organizations
+puts "Votre balance est de #{orga["organization"]["bank_accounts"][0]["balance"]}"
